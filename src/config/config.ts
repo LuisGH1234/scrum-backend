28e447e03cd31e805ef05aba093f268b1c276962
Paul <palvareze7@gmail.com>
2020-06-23 03:27:44 -0500

    Fix port on heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const Config = {
     dbPassword: process.env['DB_PASSWORD'] || 'root',
     dbPort: Number(process.env['DB_PORT'] || 3306),
     dbName: process.env['DB_NAME'] || 'ubermove',
-    port: Number(process.env['PORT'] || 3000),
+    port: Number(process.env['PORT'] || 5000),
     jwtSecretKey: process.env['JWT_SECRET_KEY'] || 'sercret-jwt-key',
     jwtExpiration: process.env['JWT_EXPIRATION'] || '7d',
 };
